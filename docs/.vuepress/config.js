e877bd554bc738c2afee486705596ca0217a4905
GAHANING <2903095411@qq.com>
2019-10-15 10:12:31 +0800

    api模块

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,115 +1,115 @@
-const { fs, path } = require('@vuepress/shared-utils');
+const { fs, path } = require("@vuepress/shared-utils");
 
 module.exports = ctx => ({
   head: [
-    ['link', { rel: 'icon', href: `/logo.png` }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ["link", { rel: "icon", href: `/logo.png` }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     [
-      'meta',
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" }
     ],
     [
-      'link',
-      { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` },
+      "link",
+      { rel: "apple-touch-icon", href: `/icons/apple-touch-icon-152x152.png` }
     ],
     [
-      'link',
+      "link",
       {
-        rel: 'mask-icon',
-        href: '/icons/safari-pinned-tab.svg',
-        color: '#3eaf7c',
-      },
+        rel: "mask-icon",
+        href: "/icons/safari-pinned-tab.svg",
+        color: "#3eaf7c"
+      }
     ],
     [
-      'meta',
+      "meta",
       {
-        name: 'msapplication-TileImage',
-        content: '/icons/msapplication-icon-144x144.png',
-      },
+        name: "msapplication-TileImage",
+        content: "/icons/msapplication-icon-144x144.png"
+      }
     ],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }]
   ],
   // theme: '@vuepress/vue',
   themeConfig: {
-    repo: 'ynu/ynu-api-docs',
+    repo: "ynu/ynu-api-docs",
     editLinks: true,
-    docsDir: 'docs',
+    docsDir: "docs",
     locales: {
-      '/': {
-        label: '简体中文',
-        selectText: '选择语言',
-        editLinkText: '在 GitHub 上编辑此页',
-        lastUpdated: '上次更新',
-        nav: require('./nav/zh'),
+      "/": {
+        label: "简体中文",
+        selectText: "选择语言",
+        editLinkText: "在 GitHub 上编辑此页",
+        lastUpdated: "上次更新",
+        nav: require("./nav/zh"),
         sidebarDepth: 3,
         sidebar: {
-          '/guide/': getGuideSidebar('指南', '深入'),
-          '/esop/':  [
+          "/guide/": getGuideSidebar("指南", "深入"),
+          "/esop/": [
             {
-              title: '基础',
+              title: "基础",
               collapsable: false,
-              children: [
-                '',
-                'introduction',
-                'admin-manual',
-              ],
+              children: ["", "introduction", "admin-manual"]
             },
             {
-              title: '深入',
+              title: "深入",
               collapsable: false,
-              children: [
-                'developer-manual',
-              ],
+              children: ["developer-manual"]
             },
             {
-              title: '其他',
+              title: "API文档",
               collapsable: false,
-              children: [
-                'markdown-getting-started',
-              ],
+              children: ["api-manual"]
+            },
+            {
+              title: "其他",
+              collapsable: false,
+              children: ["markdown-getting-started"]
             }
-          ],
-        },
-      },
-    },
+          ]
+        }
+      }
+    }
   },
   plugins: [
-    ['@vuepress/back-to-top', true],
+    ["@vuepress/back-to-top", true],
     [
-      '@vuepress/pwa',
+      "@vuepress/pwa",
       {
         serviceWorker: true,
-        updatePopup: true,
-      },
+        updatePopup: true
+      }
     ],
-    ['@vuepress/medium-zoom', true],
+    ["@vuepress/medium-zoom", true],
     [
-      'container',
+      "container",
       {
-        type: 'vue',
+        type: "vue",
         before: '<pre class="vue-container"><code>',
-        after: '</code></pre>',
-      },
+        after: "</code></pre>"
+      }
     ],
     [
-      'container',
+      "container",
       {
-        type: 'upgrade',
+        type: "upgrade",
         before: info => `<UpgradePath title="${info}">`,
-        after: '</UpgradePath>',
-      },
+        after: "</UpgradePath>"
+      }
     ],
-    'flowchart',
-    ['mathjax', {
-      target: 'svg',
-      macros: {
-        '*': '\\times',
-      },
-    }],
+    "flowchart",
+    [
+      "mathjax",
+      {
+        target: "svg",
+        macros: {
+          "*": "\\times"
+        }
+      }
+    ]
   ],
-  extraWatchFiles: ['.vuepress/nav/zh.js'],
+  extraWatchFiles: [".vuepress/nav/zh.js"]
 });
 
 function getGuideSidebar(groupA, groupB) {
@@ -118,26 +118,26 @@ function getGuideSidebar(groupA, groupB) {
       title: groupA,
       collapsable: false,
       children: [
-        '',
-        'getting-started',
-        'directory-structure',
-        'basic-config',
-        'assets',
-        'markdown',
-        'using-vue',
-        'i18n',
-        'deploy',
-      ],
+        "",
+        "getting-started",
+        "directory-structure",
+        "basic-config",
+        "assets",
+        "markdown",
+        "using-vue",
+        "i18n",
+        "deploy"
+      ]
     },
     {
       title: groupB,
       collapsable: false,
       children: [
-        'frontmatter',
-        'permalinks',
-        'markdown-slot',
-        'global-computed',
-      ],
-    },
+        "frontmatter",
+        "permalinks",
+        "markdown-slot",
+        "global-computed"
+      ]
+    }
   ];
 }
---
+++ docs/esop/api-manual.md
@@ -0,0 +1,21 @@
+# API 文档
+
+## 统计研究生数量
+
+### 请求地址
+
+http://apis.ynu.edu.cn/do/api/call/count_yjs
+
+### 请求参数
+
+| 属性   | 类型   | 默认值 | 必填 | 说明       |
+| ------ | ------ | ------ | ---- | ---------- |
+| lxbz   | String |        |      | 在校标识   |
+| xbdm   | String |        |      | 性别代码   |
+| pyccm  | String |        |      | 培养层次   |
+| xslbdm | String |        |      | 学生类别   |
+| nj     | String |        |      | 年级       |
+| dszgh  | String |        |      | 导师       |
+| sflxs  | String |        |      | 是否留学生 |
+| xwlx   | String |        |      | 学位类型   |
+| mzdm   | String |        |      | 名族       |
